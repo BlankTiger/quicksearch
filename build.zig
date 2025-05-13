972973e3d619e49d702e2f84ca6ae49ca5d4a1b9
Maciej Urban <maciej-urban@outlook.com>
2025-05-13 23:17:06 +0200

    feat: run tests for the lib

--- build.zig
+++ build.zig
@@ -15,6 +15,13 @@ pub fn build(b: *std.Build) void {
         .root_module = exe_mod,
     });
 
+    const mod = b.createModule(.{
+        .root_source_file = b.path("src/lib.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("qslib", mod);
+
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -30,7 +37,15 @@ pub fn build(b: *std.Build) void {
         .root_module = exe_mod,
     });
 
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/lib.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_lib_unit_tests.step);
 }
