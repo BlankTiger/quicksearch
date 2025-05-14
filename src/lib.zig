38deb56c12d2f413ed62861b684285bc08fcf8cc
Maciej Urban <maciej-urban@outlook.com>
2025-05-14 23:33:39 +0200

    refactor: rename idx_last to col_last, eliminate duplication for +1

--- src/lib.zig
+++ src/lib.zig
@@ -10,10 +10,10 @@ fn search(alloc: mem.Allocator, haystack: []const u8, query: []const u8) ![]Sear
     var count_lines: usize = 1;
     var line_iter = mem.splitScalar(u8, haystack, '\n');
     while (line_iter.next()) |line| : (count_lines += 1) {
-        var idx_last: usize = 0;
-        while (mem.indexOfPos(u8, line, idx_last, query)) |col| {
-            idx_last = col + 1;
-            try results.append(.{ .line = count_lines, .col = col + 1 });
+        var col_last: usize = 0;
+        while (mem.indexOfPos(u8, line, col_last, query)) |col| {
+            col_last = col + 1;
+            try results.append(.{ .line = count_lines, .col = col_last });
         }
     }
 
