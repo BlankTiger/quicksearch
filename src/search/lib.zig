bf421523f470855349935642fd361ebbaff96e31
Maciej Urban <maciej-urban@outlook.com>
2025-05-16 20:28:38 +0200

    feat: linear search without mem.indexOfPos

--- src/search/lib.zig
+++ src/search/lib.zig
@@ -11,11 +11,15 @@ pub fn linear_search(alloc: mem.Allocator, haystack: []const u8, query: []const 
 
     var count_lines: usize = 1;
     var line_iter = mem.splitScalar(u8, haystack, '\n');
-    while (line_iter.next()) |line| : (count_lines += 1) {
-        var col_last: usize = 0;
-        while (mem.indexOfPos(u8, line, col_last, query)) |col| {
-            col_last = col + 1;
-            try results.append(.{ .line = count_lines, .col = col_last });
+    line_loop: while (line_iter.next()) |line| : (count_lines += 1) {
+        for (line, 0..) |_, idx_l| {
+            if (line[idx_l..].len < query.len) {
+                continue :line_loop;
+            }
+
+            if (mem.eql(u8, line[idx_l .. idx_l + query.len], query)) {
+                try results.append(.{ .line = count_lines, .col = idx_l + 1 });
+            }
         }
     }
 
