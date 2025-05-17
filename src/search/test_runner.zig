5e946efe155775b8f511a5b807857d530122b416
Maciej Urban <maciej-urban@outlook.com>
2025-05-17 13:08:30 +0200

    tests: test runner refactor

--- src/search/test_runner.zig
+++ src/search/test_runner.zig
@@ -16,19 +16,19 @@ pub fn main() !void {
             }
         }
 
-        break :t .{try list_all.toOwnedSlice(), try list_first.toOwnedSlice()};
+        break :t .{ try list_all.toOwnedSlice(), try list_first.toOwnedSlice() };
     };
 
-    try handle_search_all(search_all_tests);
-    try handle_search_first(search_first_tests);
+    std.debug.print("Running tests for search_all_fns using a custom runner\n", .{});
+    try handle_tests(search_all_tests, t_context.search_all_fns);
+    std.debug.print("Running tests for search_all_fns using a custom runner\n", .{});
+    try handle_tests(search_first_tests, t_context.search_first_fns);
 }
 
-fn handle_search_all(tests: []const std.builtin.TestFn) !void {
-    std.debug.print("Running tests for search_all_fns using a custom runner\n", .{});
-
+fn handle_tests(tests: []const std.builtin.TestFn, fns: []const t_context.FnPair) !void {
     var tests_passed: usize = 0;
     var tests_failed: usize = 0;
-    for (t_context.search_all_fns) |sfn| {
+    for (fns) |sfn| {
         const name = sfn[1];
 
         for (tests) |t| {
@@ -37,7 +37,7 @@ fn handle_search_all(tests: []const std.builtin.TestFn) !void {
             }
         }
 
-        std.debug.print("Running tests for search_all fn '{s}'\n", .{name});
+        std.debug.print("Running tests for fn '{s}'\n", .{name});
 
         for (tests) |t| {
             if (is_setup(t.name)) continue;
@@ -58,38 +58,7 @@ fn handle_search_all(tests: []const std.builtin.TestFn) !void {
     std.debug.print(
         "Ran {d} tests. Passed: {d}, failed: {d}\n\n",
         .{
-            tests.len * t_context.search_all_fns.len,
-            tests_passed,
-            tests_failed,
-        },
-    );
-}
-
-fn handle_search_first(tests: []const std.builtin.TestFn) !void {
-    std.debug.print("Running tests for search_first_fns using a custom runner\n", .{});
-
-    var tests_passed: usize = 0;
-    var tests_failed: usize = 0;
-
-    for (tests) |t| {
-        if (is_setup(t.name)) continue;
-
-        t.func() catch |err| {
-            if (@errorReturnTrace()) |trace| std.debug.dumpStackTrace(trace.*);
-            std.debug.print(
-                "Test '{s}' failed with an error: {}\n",
-                .{ t.name, err },
-            );
-            tests_failed += 1;
-            continue;
-        };
-        tests_passed += 1;
-    }
-
-    std.debug.print(
-        "Ran {d} tests. Passed: {d}, failed: {d}\n\n",
-        .{
-            tests.len,
+            tests.len * fns.len,
             tests_passed,
             tests_failed,
         },
