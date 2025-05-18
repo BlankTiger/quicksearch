d089731e2aeaa4355a98840f1ed7e3b2b5182586
Maciej Urban <maciej-urban@outlook.com>
2025-05-19 00:34:37 +0200

    fix: benchmarks

--- src/lib.zig
+++ src/lib.zig
@@ -1,4 +1,5 @@
 pub const SearchResult = search.SearchResult;
+pub const ResultHandler = search.ResultHandler;
 pub const linear_search = search.linear_search;
 pub const linear_std_search = search.linear_std_search;
 pub const simd_search = search.simd_search;
