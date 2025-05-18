40b9345e42e1ed3061ad087c62b8997b2ebe618f
Maciej Urban <maciej-urban@outlook.com>
2025-05-18 23:22:28 +0200

    fix: handle ResultHandler

--- src/search/test_context.zig
+++ src/search/test_context.zig
@@ -1,9 +1,4 @@
-pub const FnType = *const fn (
-    std.mem.Allocator,
-    []const u8,
-    []const u8,
-) anyerror![]SearchResult;
-
+pub const FnType = *const fn (*ResultHandler, []const u8, []const u8) void;
 pub const FnPair = struct { FnType, []const u8 };
 
 pub const search_fns: []const FnPair = &[_]FnPair{
@@ -11,6 +6,7 @@ pub const search_fns: []const FnPair = &[_]FnPair{
     .{ search.simd_search, "simd_search" },
 };
 
+const ResultHandler = @import("ResultHandler.zig");
 const SearchResult = @import("SearchResult.zig");
 const std = @import("std");
 const search = @import("search.zig");
