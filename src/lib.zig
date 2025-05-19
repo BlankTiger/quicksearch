d55cdd66899885b2ba3a174967fc0bfd2c3c9d69
Maciej Urban <maciej-urban@outlook.com>
2025-05-19 23:03:10 +0200

    tests: ref files in lib

--- src/lib.zig
+++ src/lib.zig
@@ -4,4 +4,15 @@ pub const linear_search = search.linear_search;
 pub const linear_std_search = search.linear_std_search;
 pub const simd_search = search.simd_search;
 
+test {
+    // NOTE: DO NOT MENTION SEARCH HERE BECAUSE IT HAS ITS OWN
+    // CUSTOM TEST RUNNER SETUP
+    _ = MmapReader;
+    _ = Ignorer;
+    _ = fs_search;
+}
+
 pub const search = @import("search.zig");
+pub const MmapReader = @import("MmapReader.zig");
+pub const Ignorer = @import("Ignorer.zig");
+pub const fs_search = @import("fs_search.zig");
