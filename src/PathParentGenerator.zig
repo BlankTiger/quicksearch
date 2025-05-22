c2e5326ddbc33185353071efb35bc2b41fa0134d
Maciej Urban <maciej-urban@outlook.com>
2025-05-22 19:38:41 +0200

    refactor: PathParentGenerator to a struct file

--- src/PathParentGenerator.zig
+++ src/PathParentGenerator.zig
@@ -1,37 +1,37 @@
-pub const PathParentGenerator = struct {
-    path: []const u8,
-    path_part: []const u8,
-    idx_parent: usize = 0,
-    absolute: bool = false,
+const PathParentGenerator = @This();
 
-    pub fn init(path: []const u8) PathParentGenerator {
-        std.debug.assert(path.len > 2);
-        std.debug.assert(std.mem.startsWith(u8, path, "/") or std.mem.startsWith(u8, path, "./"));
+path: []const u8,
+path_part: []const u8,
+idx_parent: usize = 0,
+absolute: bool = false,
 
-        const absolute = std.mem.indexOfScalar(u8, path, '/').? == 0;
-        return .{
-            .path = path,
-            .path_part = "",
-            .idx_parent = 0,
-            .absolute = absolute,
-        };
+pub fn init(path: []const u8) PathParentGenerator {
+    std.debug.assert(path.len > 2);
+    std.debug.assert(std.mem.startsWith(u8, path, "/") or std.mem.startsWith(u8, path, "./"));
+
+    const absolute = std.mem.indexOfScalar(u8, path, '/').? == 0;
+    return .{
+        .path = path,
+        .path_part = "",
+        .idx_parent = 0,
+        .absolute = absolute,
+    };
+}
+
+pub fn next(self: *PathParentGenerator) ?[]const u8 {
+    std.debug.assert(self.path_part.len <= self.path.len);
+    if (self.path_part.len == self.path.len) return null;
+
+    const maybe_idx = std.mem.indexOfScalar(u8, self.path[self.idx_parent..], '/');
+    if (maybe_idx) |idx| {
+        const offset_idx_parent = idx + 1;
+        self.idx_parent += offset_idx_parent;
+        self.path_part = self.path[0..self.idx_parent];
+        return self.path_part;
     }
-
-    pub fn next(self: *PathParentGenerator) ?[]const u8 {
-        std.debug.assert(self.path_part.len <= self.path.len);
-        if (self.path_part.len == self.path.len) return null;
-
-        const maybe_idx = std.mem.indexOfScalar(u8, self.path[self.idx_parent..], '/');
-        if (maybe_idx) |idx| {
-            const offset_idx_parent = idx + 1;
-            self.idx_parent += offset_idx_parent;
-            self.path_part = self.path[0..self.idx_parent];
-            return self.path_part;
-        }
-        self.path_part = self.path;
-        return self.path;
-    }
-};
+    self.path_part = self.path;
+    return self.path;
+}
 
 test "relative" {
     const expected: []const []const u8 = &.{
