83957d520c05c1fea629ca1063b8e9f71f427ef1
Maciej Urban <maciej-urban@outlook.com>
2025-05-22 19:36:42 +0200

    feat: ignore rules struct stubs

--- src/GitIgnorer.zig
+++ src/GitIgnorer.zig
@@ -6,7 +6,42 @@ cache: Cache,
 allocator: std.mem.Allocator,
 
 const Cache = std.StringHashMap(?IgnoreRules);
-const IgnoreRules = struct {};
+
+const IgnoreRules = struct {
+    rules: Rules,
+    allocator: std.mem.Allocator,
+
+    const Rules = std.ArrayList(Rule);
+    const Self = @This();
+
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return .{
+            .rules = .init(allocator),
+            .allocator = allocator,
+        };
+    }
+
+    pub fn init_with(allocator: std.mem.Allocator, rule: Rule) !Self {
+        var rules: Rules = try .initCapacity(allocator, 1);
+        rules.appendAssumeCapacity(rule);
+        return .{
+            .rules = rules,
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: Self) void {
+        self.rules.deinit();
+    }
+
+    /// `rules` always overwrite existing `self.rules`
+    pub fn merge(self: *Self, rules: IgnoreRules) !void {
+
+    }
+
+    pub fn match(self: Self, path: []const u8) bool {}
+};
+
 const Rule = struct {};
 const Self = @This();
 
