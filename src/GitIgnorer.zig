df617508f63cd4eba51ccfb353b885d7c9b7a354
Maciej Urban <maciej-urban@outlook.com>
2025-05-24 16:36:37 +0200

    tests: handle patterns starting with #

--- src/GitIgnorer.zig
+++ src/GitIgnorer.zig
@@ -199,12 +199,15 @@ const ParserTests = struct {
             \\file_a.txt
             \\# file_b.txt
             \\#file_b.txt
+            \\\#file_b.txt
         );
         defer rules.deinit();
 
-        try t.expectEqual(1, rules.items.len);
+        try t.expectEqual(2, rules.items.len);
         try t.expectEqual(1, rules.items[0].parts.len);
+        try t.expectEqual(1, rules.items[1].parts.len);
         try t.expectEqualStrings("file_a.txt", rules.items[0].parts[0].literal);
+        try t.expectEqualStrings("#file_b.txt", rules.items[1].parts[0].literal);
     }
 
     const Part = Rules.Rule.Part;
