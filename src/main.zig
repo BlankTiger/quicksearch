8ba27c2948435e4ecf1b7c71649fd8b2b77f9304
Maciej Urban <maciej-urban@outlook.com>
2025-05-31 22:04:35 +0200

    feat: main

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,23 @@
 pub fn main() !void {
-    std.debug.print("hello, world\n", .{});
+    var arena_state = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena_state.deinit();
+    const allocator = arena_state.allocator();
+
+    const queue: Queue = .init(allocator);
+    var opts: finder.Options = .{
+        .path = "./src",
+        .allocator = allocator,
+        .gitignorer = .init(&arena_state),
+        .collector = queue,
+    };
+    const collector = try finder.find_files(&opts);
+    for (collector.items()) |f| std.debug.print("found: {s}\n", .{f});
+    std.debug.print("T: {s}\n", .{@typeName(@TypeOf(collector))});
 }
 
 const std = @import("std");
+const lib = @import("qslib");
+const fss = lib.fs_search;
+const Queue = fss.Queue([]const u8);
+// const Queue = fss.PathList;
+const finder = fss.Finder(Queue);
