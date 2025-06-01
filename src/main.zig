4c1636a7373b8a915621944ed7195b0592144be0
Maciej Urban <maciej-urban@outlook.com>
2025-06-01 11:25:30 +0200

    feat: multithreaded main, finally usable!

--- src/main.zig
+++ src/main.zig
@@ -3,16 +3,25 @@ pub fn main() !void {
     defer arena_state.deinit();
     const allocator = arena_state.allocator();
 
-    const queue: Queue = .init(allocator);
+    const args = try std.process.argsAlloc(allocator);
+    const cli_opts = parse_args(args);
+
     var opts: finder.Options = .{
-        .path = "./src",
+        .path = cli_opts.path,
+        .extension = cli_opts.file_type,
         .allocator = allocator,
         .gitignorer = .init(&arena_state),
-        .collector = queue,
+        .collector = Queue.init(allocator),
     };
-    const collector = try finder.find_files(&opts);
-    for (collector.items()) |f| std.debug.print("found: {s}\n", .{f});
-    std.debug.print("T: {s}\n", .{@typeName(@TypeOf(collector))});
+    const finder_thread = try run_finder(&opts);
+    defer {
+        finder_thread.join();
+        finished = true;
+    }
+    try run_search(cli_opts.needle, &opts);
+    // for (collector.items()) |f| std.debug.print("found: {s}\n", .{f});
+    // std.debug.print("T: {s}\n", .{@typeName(@TypeOf(collector))});
+}
 
 fn parse_args(args: []const []const u8) struct { path: []const u8, file_type: []const u8, needle: []const u8 } {
     if (args.len < 2) @panic("must provide at least a search query");
@@ -53,6 +62,48 @@ fn parse_args(args: []const []const u8) struct { path: []const u8, file_type: []
     };
 }
 
+var finished = false;
+
+fn run_finder(opts: *finder.Options) !std.Thread {
+    return try .spawn(.{}, finder.find_files, .{opts});
+}
+
+const MAX_THREAD_COUNT = 32;
+
+fn run_search(needle: []const u8, opts: *finder.Options) !void {
+    const count_threads = @min(try std.Thread.getCpuCount(), MAX_THREAD_COUNT);
+    var threads: [MAX_THREAD_COUNT]std.Thread = undefined;
+    var result_handler: ResultHandler = .init(std.io.getStdOut().writer().any(), .{
+        .handling_type = .vimgrep,
+    });
+
+    for (0..count_threads) |idx_thread| {
+        threads[idx_thread] = try .spawn(.{}, search_thread, .{ needle, &result_handler, &opts.collector.? });
+    }
+
+    // TODO: make this wait based on some condvar or something like this
+    for (0..count_threads) |idx_thread| {
+        threads[idx_thread].join();
+    }
+}
+
+fn search_thread(needle: []const u8, result_handler: *ResultHandler, collector: *Queue) void {
+    var arena: std.heap.ArenaAllocator = .init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const cwd = std.fs.cwd();
+    while (!finished and collector.len() > 0) {
+        while (collector.pop()) |path| {
+            defer std.debug.assert(arena.reset(.retain_capacity));
+
+            const f = cwd.openFile(path, .{}) catch @panic("couldnt open a file");
+            defer f.close();
+            const haystack = f.readToEndAlloc(arena.allocator(), comptime std.math.maxInt(usize)) catch {
+                @panic("couldnt read file to the end");
+            };
+            lib.search.search.simd_search(result_handler, path, haystack, needle);
+        }
+    }
 }
 
 const std = @import("std");
@@ -61,3 +112,4 @@ const fss = lib.fs_search;
 const Queue = fss.Queue([]const u8);
 // const Queue = fss.PathList;
 const finder = fss.Finder(Queue);
+const ResultHandler = lib.ResultHandler;
