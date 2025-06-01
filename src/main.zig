aed7a790d18c74015b9e3a8b41c84227fd366b7a
Maciej Urban <maciej-urban@outlook.com>
2025-06-01 11:25:14 +0200

    feat: arg parser

--- src/main.zig
+++ src/main.zig
@@ -13,6 +13,46 @@ pub fn main() !void {
     const collector = try finder.find_files(&opts);
     for (collector.items()) |f| std.debug.print("found: {s}\n", .{f});
     std.debug.print("T: {s}\n", .{@typeName(@TypeOf(collector))});
+
+fn parse_args(args: []const []const u8) struct { path: []const u8, file_type: []const u8, needle: []const u8 } {
+    if (args.len < 2) @panic("must provide at least a search query");
+
+    const count_additional_args = args.len - 2;
+    // TODO: make this parser good
+    if (count_additional_args % 2 != 0) @panic("additional arguments must be provided in pairs");
+
+    var path: []const u8 = "";
+    var file_type: []const u8 = "";
+    var err_buf: [20]u8 = undefined;
+    if (count_additional_args > 0) {
+        for (1..count_additional_args + 1) |idx_arg| {
+            if (idx_arg % 2 == 0) continue;
+
+            const arg = args[idx_arg];
+            std.debug.assert(arg[0] == '-');
+            switch (arg[1]) {
+                't' => {
+                    file_type = args[idx_arg + 1];
+                },
+
+                'g' => {
+                    path = args[idx_arg + 1];
+                },
+
+                else => @panic(std.fmt.bufPrint(&err_buf, "no flag: {c}", .{arg[1]}) catch "deez"),
+            }
+        }
+    }
+    // last argument is always the query
+    const needle = args[args.len - 1];
+
+    return .{
+        .path = path,
+        .file_type = file_type,
+        .needle = needle,
+    };
+}
+
 }
 
 const std = @import("std");
